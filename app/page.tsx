dd3c4c959cdfd96818865713425bf78d97062637
hayden <haydenshelm@gmail.com>
2024-05-27 15:09:37 -0700

    remove posts

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,9 @@ import Landing from "@/components/landing";
 
 export default async function Home() {
 
-  const directory = path.resolve('./app/posts');
-  const fileNames = fs.readdirSync(directory);
-  const slugs = fileNames
-      .filter((fileName: string) => fileName.endsWith('mdx'))
-      .map((fileName) => fileName.replace('.mdx', ''));
-  const metas = await Promise.all(slugs.map((slug) => getPostMeta(slug)));
-
   return (
       <div>
-          <Landing slugs={slugs} metas={metas}/>
+          <Landing />
       </div>
   );
-}
-
-async function getPostMeta(post: string) {
-    const mod = await import(`./posts/${post}.mdx`);
-    return mod.meta;
 }
