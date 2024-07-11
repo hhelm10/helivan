7d8586b8670d2673a179504656bb76333d55f476
hayden <haydenshelm@gmail.com>
2024-07-11 16:02:16 -0700

    add general sillm to home page

--- components/landing.tsx
+++ components/landing.tsx
@@ -20,16 +20,19 @@ export default function Landing() {
                 </span>
               </p>
             </div>
-            <div className="mt-10 text-8xl text-blue-6">
+            <div className="text-8xl text-blue-6">
                 <center><p> Helivan // Research</p></center>
             </div> 
+            <div className="items-center justify-center mt-6">
+                <center> <img src={"/general.svg"} width='25%'/> </center>
+            </div>
             <div className="text-2xl text-blue-4 italic mt-4">
                 <center> <p> principled statistics, next-gen applications </p> </center>
             </div>
-            <div className="margin-top-30 text-xl text-blue-4 text-center">
+            <div className="mt-2text-xl text-blue-4 text-center">
              <p> In collaboration with </p>
             </div>
-            <div className="mt-6 flex flex-row items-center justify-center gap-50">
+            <div className="mt-4 flex flex-row items-center justify-center gap-50">
                 <img src={"/msr_logo.png"} width='225px'/>
                 <img src={"/nomic_dark_logo.png"} width='120px'/>
                 <img src={"/jhu-logo.png"} width='425px'/>
