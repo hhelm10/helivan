86f865a4da01717c76118c6cb9809a66ddd76b29
kenziemac7 <macdonaldkenzie@me.com>
2025-01-22 21:25:41 -0800

    Revert "Revert "Revert "Create invite list on website"""

--- README.md
+++ README.md
@@ -4,12 +4,5 @@
 > experience them, produce flow conjunctions here and there, try out
 > continuums of intensities segment buy segment, **have a small plot of new
 > land at all times**"
-
-Deleuze & Guattari, _A Thousand Plateaus_ (161) " - Brandon Duderstadt
-
-Project set-up (all from root)
-
-1. npm i
-2. Create a .env file at the root of the project and add the DATABASE_URL
-3. npm run prisma generate
-4. npm run dev
+ 
+ Deleuze & Guattari, *A Thousand Plateaus* (161) " - Brandon Duderstadt
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,24 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Lora } from "next/font/google";
-const font = Lora({ subsets: ["latin"] });
+import './globals.css'
+import type { Metadata } from 'next'
+import { Lora } from 'next/font/google'
+import Header from "@/components/header";
+import BackButton from "@/components/back-button";
+const font = Lora({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Helivan Research",
-};
+  title: 'Helivan Research',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
