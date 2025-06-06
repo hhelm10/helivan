8d69d702ddc5ea858161562a7a6d3e78beb863bc
Kenzie Macdonald <kenziemacdonald@Kenzies-MacBook-Pro.local>
2025-06-06 12:01:01 -0400

    typeform

---
+++ components/TypeformEmbed.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+export default function TypeformEmbed() {
+  return (
+    <iframe
+      src="https://form.typeform.com/to/Z8v7tnCL"
+      style={{ 
+        width: '100%',
+        height: '400px',
+        border: 'none',
+      }}
+      allow="camera; microphone; autoplay; encrypted-media; fullscreen;"
+    />
+  )
+}
